c173f0657b1f67d28f2c600e9a7c36a2a2d0f7cd
Amirrr1987 <Maghami.A1987@gmail.com>
2020-09-15 15:51:50 +0430

    add axios tages in Apps.script

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,27 @@
 <template>
   <body id="app">
     <section class="periodic">
-      
+      {{info}}
     </section>
   </body>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   name: 'App',
+
+
+  el: '#app',
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  }
 }
 </script>
